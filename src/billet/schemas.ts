c850cfbcbf9813cdbeccea0c042fd28b9f74295d
JVictorV <joaovictorvisona@gmail.com>
2020-09-25 17:33:04 -0300

    Fixing zod imports

--- src/billet/schemas.ts
+++ src/billet/schemas.ts
@@ -1,4 +1,4 @@
-import z from 'zod';
+import * as z from 'zod';
 import { zRequest } from '../shared/schemas';
 
 const zBoletoInstrucoes = z.object({
--- src/transfer/schemas.ts
+++ src/transfer/schemas.ts
@@ -1,4 +1,4 @@
-import z from 'zod';
+import * as z from 'zod';
 import { zRequest } from '../shared/schemas';
 
 export const zTransferenciaRequest = zRequest;
